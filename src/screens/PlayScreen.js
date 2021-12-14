99dec8f609a015897eab35d437a08315c1e6f45b
jamesvinnicombe <80378047+jvinn@users.noreply.github.com>
2021-12-14 12:30:20 +1100

    Implemented scoring logic for correct guesses and skipped flags

--- src/screens/PlayScreen.js
+++ src/screens/PlayScreen.js
@@ -1,8 +1,29 @@
-import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
-import {flags} from '../assets/CountryFlagsArray';
+import React, {useEffect, useState} from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import {countries} from '../assets/CountryArray';
+import fonts from '../styles/Fonts';
 
 function PlayScreen({navigation}) {
+  const [country, setCountry] = useState(
+    countries[Math.floor(Math.random() * 261)],
+  );
+  const [score, setScore] = useState(0);
+  const [skipped, setSkipped] = useState(0);
+  const [text, setText] = useState(null);
+
+  function newCountry() {
+    setText('');
+    countries.splice(countries.indexOf(country), 1);
+    let randIndex = Math.floor(Math.random() * countries.length);
+    setCountry(countries[randIndex]);
+  }
+
   const styles = StyleSheet.create({
     playScreen: {
       flex: 1,
@@ -10,15 +31,44 @@ function PlayScreen({navigation}) {
       justifyContent: 'center',
     },
     flag: {
-      fontSize: 200,
+      fontSize: 300,
+    },
+    progress: {
+      fontFamily: 'avenir',
+      fontSize: 14,
     },
   });
 
   return (
     <View style={styles.playScreen}>
-      {flags.map(flag => (
-        <Text>{flag.emoji}</Text>
-      ))}
+      {261 - countries.length > 0 ? (
+        <Text style={fonts.title}>
+          {((score / (261 - countries.length)) * 100).toFixed(0)}%
+        </Text>
+      ) : null}
+      <Text style={fonts.heading}>{261 - countries.length} / 261</Text>
+      <Text style={styles.flag}>{country.emoji}</Text>
+      <View style={fonts.buttonBorder}>
+        <TextInput
+          style={fonts.buttonText}
+          onChangeText={text => {
+            setText(text);
+            if (text === country.name) {
+              setScore(score + 1);
+              newCountry();
+            }
+          }}
+          value={text}
+        />
+      </View>
+      <TouchableOpacity
+        onPress={() => {
+          setSkipped(skipped + 1);
+          newCountry();
+        }}>
+        <Text style={fonts.buttonBorder}>Skip</Text>
+      </TouchableOpacity>
+      {console.log(country.name)}
     </View>
   );
 }
---
+++ src/styles/Fonts.js
@@ -0,0 +1,27 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  title: {
+    fontWeight: 'bold',
+    fontSize: 40,
+    fontFamily: 'avenir',
+  },
+  buttonText: {
+    fontFamily: 'avenir',
+    fontSize: 20,
+  },
+  buttonBorder: {
+    padding: 10,
+    borderWidth: 1,
+    marginBottom: 20,
+    borderColor: 'black',
+    borderRadius: 10,
+    width: '60%',
+  },
+  heading: {
+    fontFamily: 'avenir',
+    fontSize: 20,
+    padding: 10,
+    fontWeight: 'bold',
+  },
+});
